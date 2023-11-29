6f409f4f8059ffa071011ccc1368c0392674bea7
Kavya-1219 <132338691+Kavya-1219@users.noreply.github.com>
2023-11-29 10:53:04 +0530

    Add files via upload

---
+++ decimal to hexa.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int decimalNumber;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimalNumber);
+
+    printf("hexadecimal equivalent: %x\n", decimalNumber);
+
+    return 0;
+}
